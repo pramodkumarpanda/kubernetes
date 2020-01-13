ec8bfc028283d0941e6adaf974cca8e0a7484cb3
pramodkumarpanda <31403290+pramodkumarpanda@users.noreply.github.com>
2020-01-13 17:47:04 +0530

    Delete new_master_create.sh

--- automation_k8s/new_master_create.sh
+++
@@ -1,2 +0,0 @@
-kubeadm join kubeadm join localhost:6443 --token annfs5.07umrl71phzudhbx     --discovery-token-ca-cert-hash sha256:dfbabdb17dece6ff119578ed199b47a7f996b1613c5bb8d6d030e0d93a0dae1f    
-    --control-plane --certificate-key 0ada5abed74e4e661599eb8e8d771ea4e1c0356543782f1892be34fab9bec0a9
